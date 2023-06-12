89e0878f54cc527ff193925a558d8da60e14364f
MauryUSM <128706394+MauryUSM@users.noreply.github.com>
2023-06-11 20:16:33 -0400

    Update MainActivity.java
    Se añadieron nuevos productos

--- frontend/MyApplication3/app/src/main/java/com/example/myapplication3/MainActivity.java
+++ frontend/MyApplication3/app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -26,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
         productList.add(new Product("Producto 1", "Descripción del Producto 1", R.drawable.zapa1));
         productList.add(new Product("Producto 2", "Descripción del Producto 2", R.drawable.zapa2));
         productList.add(new Product("Producto 3", "Descripción del Producto 3", R.drawable.zapa3));
+        productList.add(new Product("Producto 4", "Zapatillas para correr en todo terreno", R.drawable.zapa4));
+        productList.add(new Product("Producto 5", "Zapatillas que te harán ser la estrella de la fiesta", R.drawable.zapa5));
+        productList.add(new Product("Producto 6", "Zapatillas perfectas para tener el mayor agarre en la cancha ", R.drawable.zapa6));
+        productList.add(new Product("Producto 7", "Zapatillas impermeables perfectas para este invierno", R.drawable.zapa7));
         // Agrega más productos según necesites
 
         // Crear el adaptador y asignarlo al RecyclerView
